c58b24aee86ac821d7a4849448ec091ada35227f
Mantong Zhao <zhaomantong0203@gmail.com>
2019-06-26 11:39:28 -0700

    commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import HelloWorld from './state-drills/HelloWorld';
+import Bomb from './state-drills/Bomb';
+
+function App() {
+  return (
+    <main className='App'>
+      <HelloWorld />
+      <Bomb />
+    
+      {/* content goes here */}
+    </main>
+  );
+}
+
+export default App;
