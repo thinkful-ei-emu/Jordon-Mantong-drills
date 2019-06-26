8c1ce75a631d7e0b245523f922ff5b8e27157620
Jvymvn <Jordon.carter@yahoo.com>
2019-06-26 17:26:02 -0400

    commit

--- src/accordion/Accordion.js
+++ src/accordion/Accordion.js
@@ -5,15 +5,34 @@ export default class Accordion extends Component {
         current: null
     }
 
+    listenEvent(tab) {
+        this.setState({ current: tab })
+    }
+
+    buttons() {
+        return this.props.sections.map((section, key) => (
+            <li key={key} onClick={() => this.listenEvent(key)}>
+                <button>{section.title}</button>
+                {this.renderContent()}
+            </li >
+        ))
+    }
+
+
+    renderContent() {
+        let currentTab = this.props.sections[this.state.current];
+        return (
+            <p>
+                {currentTab.content}
+            </p>
+        )
+    }
+
     render() {
         console.log(this.props.sections);
         return (
             <ul>
-                {this.props.sections.map((section, key) => (
-                    <li key={key}>
-                        <button>{section.title}</button>
-                    </li>
-                ))}
+                {this.buttons()}
             </ul>
         )
     }
